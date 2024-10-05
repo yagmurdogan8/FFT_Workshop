943accc7d76a832969966ce718612245c43a3cff
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2024-10-06 01:55:26 +0300

    first task and audio files

--- fft_workshop.py
+++ fft_workshop.py
@@ -2,3 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import librosa
 import librosa.display
+
+y, sr = librosa.load(librosa.ex('piano.wav'))
+D = librosa.stft(y) # STFT of y
+S_db = librosa.amplitude_to_db(np.abs(D), ref=np.max)
+plt.figure()
+librosa.display.specshow(S_db)
+plt.colorbar()
